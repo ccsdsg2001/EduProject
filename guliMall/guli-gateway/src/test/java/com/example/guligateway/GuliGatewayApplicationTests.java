7b11f9eb21270454bdd35d48bfa93573ce86f009
NutF4n <ccsdsg2016@gmail.com>
2023-02-20 00:50:58 +0800

    day3

---
+++ guliMall/guli-gateway/src/test/java/com/example/guligateway/GuliGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guligateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

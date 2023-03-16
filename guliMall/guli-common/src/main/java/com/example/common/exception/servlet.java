2ff6f0423db07022e1d380c293cc9e8cce6e0f0e
NutF4n <ccsdsg2016@gmail.com>
2023-03-16 23:46:31 +0800

    d27 stress test

---
+++ guliMall/guli-common/src/main/java/com/example/common/exception/servlet.java
@@ -0,0 +1,28 @@
+package com.example.common.exception;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author cc
+ * @date 2023年03月16日 19:06
+ */
+public class servlet  extends HttpServlet {
+    public servlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
